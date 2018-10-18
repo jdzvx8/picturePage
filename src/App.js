49e189dc79a6bd30347da2908f8725e22cafe107
Jason <shaggy9869@hotmail.com>
2018-10-17 20:03:52 -0500

    update to fix repeat cycle of pics, add pics of parker

--- src/App.js
+++ src/App.js
@@ -4,32 +4,50 @@ import Lightbox from 'react-image-lightbox'
 import './App.css';
 import 'react-image-lightbox/style.css'
 
-const images = ['https://cdn.cnn.com/cnnnext/dam/assets/170407220916-04-iconic-mountains-matterhorn-restricted.jpg',
-'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Nanga_Parbat_The_Killer_Mountain.jpg/250px-Nanga_Parbat_The_Killer_Mountain.jpg']
+const images = ['https://imgur.com/YTum3v4.jpg', 'https://cdn.cnn.com/cnnnext/dam/assets/170407220916-04-iconic-mountains-matterhorn-restricted.jpg',
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Nanga_Parbat_The_Killer_Mountain.jpg/250px-Nanga_Parbat_The_Killer_Mountain.jpg', 'https://imgur.com/YTum3v4.jpg']
 
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      photoIndex: 0
+      photoIndex: 0,
+      isOpen: false
     }
   }
 
   render() {
-    let { photoIndex } = this.state
+    let { photoIndex, isOpen } = this.state
     console.log('test', photoIndex)
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Lightbox
-            mainSrc={images[photoIndex]}
-            nextSrc={images[++photoIndex]}
-            prevSrc={images[--photoIndex]}
-            onMovePreRequest={() => (this.setState({ photoIndex: (photoIndex + images.length - 1) % images.length }))}
-            onMoveNextRequest={() => (this.setState({ photoIndex: (photoIndex + 1) % images.length }))}
-          />
+          <div>
+            <button type="button" onClick={() => this.setState({ isOpen: true })}>
+              Open Lightbox
+            </button>
+
+            {isOpen && (
+              <Lightbox
+                mainSrc={images[photoIndex]}
+                nextSrc={images[(photoIndex + 1) % images.length]}
+                prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+                onCloseRequest={() => this.setState({ isOpen: false })}
+                onMovePrevRequest={() =>
+                  this.setState({
+                    photoIndex: (photoIndex + images.length - 1) % images.length,
+                  })
+                }
+                onMoveNextRequest={() =>
+                  this.setState({
+                    photoIndex: (photoIndex + 1) % images.length,
+                  })
+                }
+              />
+            )}
+          </div>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
