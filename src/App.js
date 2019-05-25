a9cadfb73a4514400cbf3a4dfb3c3516974dc24a
Jason <shaggy9869@yahoo.com>
2019-05-25 02:04:20 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Gallery from 'react-grid-gallery';
+
+// TODO
+// 1. Wrap in Security
+// 2. move images to another location
+// 3. dynamically upload images
+// 4. 
+
+const IMAGES =
+[{
+        src: "https://imgur.com/YTum3v4.jpg",
+        thumbnail: "https://imgur.com/YTum3v4.jpg",
+}
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div>
+      <Gallery images={IMAGES}/>
+        </div>
     );
   }
 }
